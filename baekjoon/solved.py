d00b7a57de028593e41aa85b0cb7d5790793b00c
Seunghwan Chun <zshchun@gmail.com>
2025-05-30 00:05:51 +0900

    Fix solved.ac random problem picking

--- baekjoon/solved.py
+++ baekjoon/solved.py
@@ -12,6 +12,15 @@ levels = ('b', 's', 'g', 'p', 'd', 'r')
 level_nums = ('5', '4', '3', '2', '1')
 level_classes = ( 'Bronze', 'Silver', 'Gold', 'Platinum', 'Diamond', 'Ruby' )
 level_roman = ( 'V', 'IV', 'III', 'II', 'I' )
+level_table = {
+    'b' : [1, 5], 's' : [6, 10], 'g' : [11, 15], 'p' : [16, 20], 'd' : [21, 25], 'r' : [26, 30],
+    'b5' : [1], 'b4' : [2], 'b3' : [3], 'b2' : [4], 'b1' : [5],
+    's5' : [6], 's4' : [7], 's3' : [8], 's2' : [9], 's1' : [10],
+    'g5' : [11], 'g4' : [12], 'g3' : [13], 'g2' : [14], 'g1' : [15],
+    'p5' : [16], 'p4' : [17], 'p3' : [18], 'p2' : [19], 'p1' : [20],
+    'd5' : [21], 'd4' : [22], 'd3' : [23], 'd2' : [24], 'd1' : [25],
+    'r5' : [26], 'r4' : [27], 'r3' : [28], 'r2' : [29], 'r1' : [30],
+}
 
 def extract_problem_from_html(tr):
     td = tr.xpath('.//td')
@@ -38,29 +47,40 @@ def lvnum_to_string(lv):
     level_name = level_classes[(lv-1)//5] + " " + level_roman[(lv-1)%5]
     return level_name
 
+
 def pick_random(args):
-    lv_start = str(args.level_start)[:2]
-    if not lv_start[0] in levels or (len(lv_start) == 2 and not lv_start[1] in level_nums):
+    lv_start = str(args.level_start)
+    lv_end = str(args.level_end)
+    if not lv_start in level_table or not lv_end in level_table:
         print("[!] Failed to parse problem level")
         return
-    if 'level_end' in args and args.level_end:
-        lv_end = str(args.level_end)[:2]
-        if not lv_end[0] in levels or (len(lv_end) == 2 and not lv_end[1] in level_nums):
-            print("[!] Failed to parse problem level")
-            return
-    else:
-        lv_end = lv_start[0]
+    lv_start = level_table[lv_start][0];
+    lv_end = level_table[lv_end][-1];
+    if lv_start > lv_end:
+        t = lv_start
+        lv_start = lv_end
+        lv_end = t
+    lv_range = "{}..{} ({:d}..{:d})".format(args.level_start, args.level_end, lv_start, lv_end)
+#    lv_start = str(args.level_start)[:2]
+#    if not lv_start[0] in levels or (len(lv_start) == 2 and not lv_start[1] in level_nums):
+#        print("[!] Failed to parse problem level")
+#        return
+#    if 'level_end' in args and args.level_end:
+#        lv_end = str(args.level_end)[:2]
+#        if not lv_end[0] in levels or (len(lv_end) == 2 and not lv_end[1] in level_nums):
+#            print("[!] Failed to parse problem level")
+#            return
+#    else:
+#        lv_end = lv_start[0]
 
-    if len(lv_start) == 1: lv_start += '5'
-    if len(lv_end) == 1: lv_end += '1'
-    lv_range = "{}..{}".format(lv_start, lv_end)
     print('[+] Random pick', lv_range)
     page = 1
     query = '*{} ~@$me'.format(lv_range)
     if 'solved_count' in args and args.solved_count:
         query += ' s#{}..'.format(args.solved_count)
     query = parse.quote_plus(query)
-    url = '{}/search?query={}&sort=random&direction=asc&page={:d}'.format(SOLVED_HOST, query, page)
+    #url = '{}/search?query={}&sort=random&direction=asc&page={:d}'.format(SOLVED_HOST, query, page)
+    url = '{}/problems?levelStart={:d}&levelEnd={:d}&state=unsolved&sort=random&direction=asc&page={:d}'.format(SOLVED_HOST, lv_start, lv_end, page)
     asyncio.run(async_query_solvedac(url, args.list))
 
 def pick_class(args):
